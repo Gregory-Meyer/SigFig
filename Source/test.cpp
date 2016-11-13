d4ecd0af6fdbeb4e74c1136fdabe5b97a52defcd
Gregory-Meyer <gregjm@umich.edu>
2016-11-12 22:59:35 -0500

    Added boolean comparison and operators
    Boolean comparisons, as well as their operator equivalents, are now
    supported. Increment and decrement, both prefix and postfix, are also
    supported. Set functions that should be marked const as such.

--- Source/test.cpp
+++ Source/test.cpp
@@ -7,20 +7,26 @@ using namespace std;
 
 int main() {
 	srand(time(NULL));
-	double r1;
+	double r1, r2;
 
 	r1 = (double) rand() / (RAND_MAX + 1.0) * 100;
 	r1 -= 50;
 
-	SigFig sf;
+	r2 = (double) rand() / (RAND_MAX + 1.0) * 100;
+	r2 -= 50;
 
-	sf = SigFig(r1, 5);	
+	SigFig sf, sf2;
 
-	cout << sf.doubleValue() << endl << endl;
+	sf = SigFig(r1, 5);
+	sf2 = SigFig(r2, 5);
 
-	cout << sf.sfFloor().doubleValue() << endl;
-	cout << sf.sfCeil().doubleValue() << endl;
-	cout << sf.sfRound().doubleValue() << endl;
-	cout << sf.sfRoundUp().doubleValue() << endl;
-	cout << sf.sfRoundDown().doubleValue() << endl;
+	cout << sf.doubleValue() << endl;
+	cout << sf2.doubleValue() << endl << endl;
+
+	cout << (sf == sf2) << endl;
+	cout << (sf != sf2) << endl;
+	cout << (sf < sf2) << endl;
+	cout << (sf > sf2) << endl;
+	cout << (sf <= sf2) << endl;
+	cout << (sf >= sf2) << endl;
 }
